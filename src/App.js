75ae5331ff750b73c0cb61221d111e4bd473cf36
DimitrisVett <54626952+DimitrisVett@users.noreply.github.com>
2022-10-06 13:13:10 +0200

    Added github actions version

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!
-          Added this line (for commit test)
-        </p>
+        <p>Bye there!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
